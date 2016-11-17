58d86121e224508c398ea76306ab4f4c8d111563
pkaczynski <none@none>
2016-11-17 16:55:18 +0100

    * Added the possibility to reference cells

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ import unittest
 from datetime import date
 
 from openpyxl.reader.excel import load_workbook
+from openpyxl.styles import Font
 
 from xml2xlsx import xml2xlsx
 
@@ -28,6 +29,25 @@ class XML2XLSXTest(unittest.TestCase):
         ws = wb.get_sheet_by_name("test")
         self.assertEquals(ws["A1"].value, u"test cell")
         self.assertEquals(ws["B1"].value, u"test cell2")
+
+    def test_cell_font_format(self):
+        template = """
+        <sheet name="test">
+            <row>
+                <cell font="size: 10; bold: True;">test cell</cell>
+            </row>
+        </sheet>
+
+        """
+        sheet = io.BytesIO(xml2xlsx(template))
+        wb = load_workbook(sheet)
+
+        self.assertEquals(len(wb.worksheets), 1,
+                          u"Created workbook should have only one sheet")
+        self.assertIn("test", wb.get_sheet_names(), u"Worksheet 'test' missing")
+        ws = wb.get_sheet_by_name("test")
+        self.assertEquals(ws["A1"].font.size, 10, "Font size not set properly")
+        self.assertTrue(ws["A1"].font.bold, "Font is not bold")
 
     def test_unicode(self):
         template = """
@@ -77,6 +97,56 @@ class XML2XLSXTest(unittest.TestCase):
         wb = load_workbook(sheet)
         ws = wb.get_sheet_by_name("test")
         self.assertEquals(ws["A1"].value.date(), date(1981, 01, 24))
+
+    def test_cell_ref_id(self):
+        template = u"""
+        <sheet name="test">
+            <row><cell ref-id="refcell">XXXX</cell></row>
+            <row><cell>{refcell}</cell></row>
+        </sheet>
+        """
+        sheet = io.BytesIO(xml2xlsx(template))
+        wb = load_workbook(sheet)
+        ws = wb.get_sheet_by_name("test")
+        self.assertEquals(ws["A2"].value, "A1")
+
+    def test_cell_ref_col(self):
+        template = u"""
+        <sheet name="test">
+            <row><cell>{col}</cell><cell>{col}</cell></row>
+        </sheet>
+        """
+        sheet = io.BytesIO(xml2xlsx(template))
+        wb = load_workbook(sheet)
+        ws = wb.get_sheet_by_name("test")
+        self.assertEquals(ws["A1"].value, "1")
+        self.assertEquals(ws["B1"].value, "2")
+
+    def test_cell_ref_row(self):
+        template = u"""
+        <sheet name="test">
+            <row><cell>{row}</cell></row>
+            <row><cell>{row}</cell></row>
+        </sheet>
+        """
+        sheet = io.BytesIO(xml2xlsx(template))
+        wb = load_workbook(sheet)
+        ws = wb.get_sheet_by_name("test")
+        self.assertEquals(ws["A1"].value, "1")
+        self.assertEquals(ws["A2"].value, "2")
+
+    def test_cell_ref_append(self):
+        template = u"""
+        <sheet name="test">
+            <row><cell ref-append="my-list">ABC</cell></row>
+            <row><cell ref-append="my-list">DEFG</cell></row>
+            <row><cell>{my-list}</cell></row>
+        </sheet>
+        """
+        sheet = io.BytesIO(xml2xlsx(template))
+        wb = load_workbook(sheet)
+        ws = wb.get_sheet_by_name("test")
+        self.assertEquals(ws["A3"].value, "A1, A2")
 
 
 if __name__ == '__main__':
--- xml2xlsx/__init__.py
+++ xml2xlsx/__init__.py
@@ -4,14 +4,25 @@ import logging
 from lxml import etree
 from datetime import datetime
 
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 
 from openpyxl import Workbook
+from openpyxl.styles import Font
 from openpyxl.writer.dump_worksheet import WriteOnlyCell
 from openpyxl.writer.excel import save_virtual_workbook
 
 logger = logging.getLogger(__name__)
 
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def excel_style(row, col):
+    """ Convert given row and column number to an Excel-style cell name. """
+    result = []
+    while col:
+        col, rem = divmod(col - 1, 26)
+        result[:0] = LETTERS[rem]
+    return ''.join(result) + str(row)
 
 class XML2XLSXTarget(object):
     def __init__(self):
@@ -21,7 +32,12 @@ class XML2XLSXTarget(object):
         self._cell = None
         self._cell_type = None
         self._cell_date_format = None
-
+        self._row = 0
+        self._col = 0
+        self._refs = {
+            'row': 1,
+            'col': 1,
+        }
 
     def start(self, tag, attrib):
         if tag == 'sheet':
@@ -31,10 +47,37 @@ class XML2XLSXTarget(object):
                 self._current_ws = self.wb.create_sheet()
             if 'name' in attrib:
                 self._current_ws.title = attrib['name']
+            self._row = 0
         elif tag == 'row':
             self._row_buf = []
+            self._col = 0
         elif tag == 'cell':
             self._cell = WriteOnlyCell(self._current_ws)
+            for attr, value in attrib.iteritems():
+                if attr == 'font':
+                    params = dict([v.split(':') for v in
+                                   value.split(';') if v.strip()])
+                    result = {}
+                    for param, value in params.iteritems():
+                        param = param.strip()
+                        value = value.strip()
+                        if value in ['True', 'False']:
+                            result[param] = bool(value)
+                        else:
+                            try:
+                                result[param] = int(value)
+                            except:
+                                result[param] = float(value)
+                    font = Font(**result)
+                    self._cell.font = font
+                elif attr == 'ref-id':
+                    self._refs[value] = excel_style(self._row + 1,
+                                                    self._col + 1)
+                elif attr == 'ref-append':
+                    self._refs[value] = self._refs.get(value, [])
+                    self._refs[value].append(excel_style(self._row + 1,
+                                                         self._col + 1))
+                    # self._refs[value].append()
             ctype = attrib.get('type', 'unicode')
             if ctype not in ['unicode', 'number', 'date']:
                 raise ValueError(u'Unknown cell type {ctype}.'.format(
@@ -62,15 +105,28 @@ class XML2XLSXTarget(object):
         elif tag == 'row':
             self._current_ws.append(self._row_buf)
             self._row_buf = []
+            self._row += 1
+            self._refs['row'] = self._row + 1
         elif tag == 'cell':
+            if self._cell.value:
+                stringified = {
+                    k: ", ".join(v) if hasattr(v, '__iter__') else v
+                    for k, v in self._refs.iteritems()
+                }
+                self._cell.value = self._cell.value.format(**stringified)
             if self._cell_type == 'number':
                 if self._cell.value:
-                    self._cell.value = Decimal(self._cell.value)
+                    try:
+                        self._cell.value = Decimal(self._cell.value)
+                    except InvalidOperation:
+                        pass
             elif self._cell_type == 'date':
                 self._cell.value = datetime.strptime(
                         self._cell.value, self._cell_date_format).date()
             self._row_buf.append(self._cell)
             self._cell = None
+            self._col += 1
+            self._refs['col'] = self._col + 1
 
     def close(self):
         return save_virtual_workbook(self.wb)
